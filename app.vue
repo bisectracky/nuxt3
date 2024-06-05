a8426e7729f57185bffe43f2929b2a1d75ecd099
Zihao <atjohore@gmail.com>
2024-06-05 21:39:13 +0900

    tailwind css init

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <h1 class="bg-red-400 text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
   },
-}
-)
+
+  modules: ["@nuxtjs/tailwindcss"]
+});
