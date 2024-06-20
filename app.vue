3d6feb3a5e67a4c6334bbe9d80e29336e4c0c40a
Zihao <atjohore@gmail.com>
2024-06-21 05:07:23 +0900

    update

--- app.vue
+++ app.vue
@@ -4,29 +4,27 @@
       <h1 class="bg-gradient-to-r from-yellow-400 from-0% via-orange-500 via-30% to-pink-500 to-90% text-4xl font-bold text-red-200">Orecchiette, Langoustine and Fennel Flower</h1>
     </body>
   </div><br>
-  <div>
-    <body class="flex items-center h-10 justify-center text-red-700">
-      <div class="md-4 bg-gradient-to-r from-yellow-400 from-0% via-orange-500 via-30% to-pink-500 to-90% text-2xl p-1>">
-        <button @click="signUp" class="inline-block w-16 px-5 py-2 font-semibold text-white hover:from-teal-400 hover:to-blue-500 text-center">
+  <div class="flex space-x-4">
+      <h1 class="bg-gradient-to-br from-yellow-400 from-0% via-orange-500 via-30% to-pink-500 to-90% text-2xl">
+        <button @click="signUp" class="inline-block w-1/2 md:w-full px-5 py-2 bg-gradient-to-br font-semibold text-white hover:from-teal-400 hover:to-blue-500">
           Sign Up
         </button>
-      </div>
-    </body>
-    <body class="flex flex-col items-center h-50 justify-center text-red-300 md-6">
+      </h1>
+  </div><br>
+  <body class="flex flex-col items-center h-50 justify-center text-red-300 md-6">
     <div class="flex space-x-4">
-    <h1>
-      <button @click="signIn" class="inline-block mb-6 bg-gradient-to-br from-blue-500 from-0% via-indigo-300 via-30% to-green-300 to-90% px-5 py-2 font-semibold text-white hover:from-teal-400 hover:to-blue-500">
+      <h1>
+        <button @click="signIn" class="inline-block mb-6 bg-gradient-to-br from-blue-500 from-0% via-indigo-300 via-30% to-green-300 to-90% px-5 py-2 font-semibold text-white hover:from-teal-400 hover:to-blue-500">
         Sign In
-      </button>
-    </h1>
-    <h1>
-      <button @click="signOut" class="inline-block mb-6 bg-gradient-to-br from-blue-500 from-0% via-indigo-300 via-30% to-green-300 to-90% px-4 py-2 font-semibold text-white hover:from-teal-400 hover:to-blue-500">
+        </button>
+      </h1>
+      <h1>
+        <button @click="signOut" class="inline-block mb-6 bg-gradient-to-br from-blue-500 from-0% via-indigo-300 via-30% to-green-300 to-90% px-4 py-2 font-semibold text-white hover:from-teal-400 hover:to-blue-500">
         Sign Out
-      </button>
-    </h1>
+        </button>
+      </h1>
     </div>
   </body>
-  </div>
 </template>
 
 <script setup>
